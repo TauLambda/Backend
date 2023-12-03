e1eafbe62a8daea23111fbb19a523796c6af2d7e
“MelFadanelli” <a01749483@tec.mx>
2023-12-03 15:10:55 -0600

    Comentarios y arreglo de indentación

--- src/controllers/HistoryController.ts
+++ src/controllers/HistoryController.ts
@@ -7,10 +7,10 @@ import db from "../models"; // Importa el objeto db desde el módulo models
  */
 class HistoryController extends AbstractController {
     /**
-   * Método para validar el cuerpo de la petición (necesario por la herencia de AbstractController).
-   * @param type - Tipo de datos para la validación del cuerpo (no implementado en este controlador).
-   * @returns Objeto de validación (no implementado en este controlador).
-   */
+     * Método para validar el cuerpo de la petición (necesario por la herencia de AbstractController).
+     * @param type - Tipo de datos para la validación del cuerpo (no implementado en este controlador).
+     * @returns Objeto de validación (no implementado en este controlador).
+     */
     protected validateBody(type: any) {
         throw new Error('Method not implemented.');
     }
@@ -18,114 +18,123 @@ class HistoryController extends AbstractController {
     private static instance: HistoryController;
 
     /**
-   * Método estático para obtener una instancia única del controlador de historiales (singleton).
-   * @returns Instancia del controlador de historiales.
-   */
-    public static getInstance():AbstractController{
-        if(this.instance){
+     * Método estático para obtener una instancia única del controlador de historiales (singleton).
+     * @returns Instancia del controlador de historiales.
+     */
+    public static getInstance(): AbstractController {
+        if (this.instance) {
             return this.instance;
-            }
+        }
         this.instance = new HistoryController('history');
         return this.instance;
     }
+
     /**
-   * Método para inicializar las rutas del controlador de historiales.
-   * Define las rutas para obtener y crear historiales.
-   */
+     * Método para inicializar las rutas del controlador de historiales.
+     * Define las rutas para obtener y crear historiales.
+     */
     protected initRoutes(): void {
         this.router.get('/readHistoryByUserId/:ID_usuario', this.getHistoryByUserId.bind(this));
-        this.router.post('/createHistory/:ID_usuario',this.postCreateHistory.bind(this));
+        this.router.post('/createHistory/:ID_usuario', this.postCreateHistory.bind(this));
         this.router.get('/currentCharge/:ID_usuario', this.getCurrentChargeByUserId.bind(this));
         this.router.get('/updateStatus/', this.updateStatusByHistoryId.bind(this));
-
     }
 
     /**
-   * Método para obtener todo el historial de un usuario.
-   * @param req - Objeto Request de Express.
-   * @param res - Objeto Response de Express.
-   */
+     * Método para obtener todo el historial de un usuario.
+     * @param req - Objeto Request de Express.
+     * @param res - Objeto Response de Express.
+     */
     private async getHistoryByUserId(req: Request, res: Response) {
         try {
             const userId = req.params.ID_usuario;
 
             // Busca todo el historial asociado al usuario
-            let historial = await db["historial"].findAll({where: { ID_usuario: userId}});
-            console.log("Historial:", historial)
+            let historial = await db["historial"].findAll({ where: { ID_usuario: userId } });
+            console.log("Historial:", historial);
             res.send(historial);
         } catch (error) {
-            if (error instanceof Error){
+            if (error instanceof Error) {
                 res.status(500).send({ message: error.message });
-                }else{
+            } else {
                 res.status(500).send({ message: "Error" });
-                }
+            }
         }
     }
 
+    /**
+     * Método para actualizar el estado de un historial por su ID.
+     * @param req - Objeto Request de Express.
+     * @param res - Objeto Response de Express.
+     */
     private async updateStatusByHistoryId(req: Request, res: Response) {
         try {
             const historyId = req.body.ID_historial;
             const newStatus = req.body.Estatus;
 
-            // Check if 'historyId' and 'newStatus' are provided
+            // Verifica si se proporcionaron 'historyId' y 'newStatus'
             if (!historyId || !newStatus) {
-                return res.status(400).send({ message: 'History ID and new status are required.' });
+                return res.status(400).send({ message: 'Se requiere el ID del historial y el nuevo estado.' });
             }
 
-            // Find the historical record by historyId
+            // Busca el registro histórico por su ID
             const historicalRecord = await db["historial"].findByPk(historyId);
 
-            // Check if the historical record exists
+            // Verifica si el registro histórico existe
             if (!historicalRecord) {
-                return res.status(404).send({ message: 'Historical record not found.' });
+                return res.status(404).send({ message: 'Registro histórico no encontrado.' });
             }
 
-            // Update the status
+            // Actualiza el estado
             historicalRecord.Estatus = newStatus;
 
-            // Save the changes to the database
+            // Guarda los cambios en la base de datos
             await historicalRecord.save();
 
-            // Send a success response
-            res.send({ message: 'Status updated successfully.' });
+            // Envía una respuesta exitosa
+            res.send({ message: 'Estado actualizado exitosamente.' });
         } catch (error) {
             if (error instanceof Error) {
                 res.status(500).send({ message: error.message });
             } else {
-                res.status(500).send({ message: 'Error updating status.' });
+                res.status(500).send({ message: 'Error al actualizar el estado.' });
             }
         }
     }
 
-
+    /**
+     * Método para obtener la carga actual de un usuario.
+     * @param req - Objeto Request de Express.
+     * @param res - Objeto Response de Express.
+     */
     private async getCurrentChargeByUserId(req: Request, res: Response) {
         try {
             const userId = req.params.ID_usuario;
             const { Op } = require("sequelize");
 
-            let historial = await db["historial"].findOne({where: {[Op.and]: [{ ID_usuario: userId},{ Estatus: "Incompleto"}]}});
+            let historial = await db["historial"].findOne({ where: { [Op.and]: [{ ID_usuario: userId }, { Estatus: "Incompleto" }] } });
             if (!historial) {
-                return res.status(404).send({ message: 'Pending charge not found.' });
+                return res.status(404).send({ message: 'Carga pendiente no encontrada.' });
             }
-            console.log("Historial:", historial)
+            console.log("Historial:", historial);
             res.send(historial);
         } catch (error) {
-            if (error instanceof Error){
+            if (error instanceof Error) {
                 res.status(500).send({ message: error.message });
-                }else{
+            } else {
                 res.status(500).send({ message: "Error" });
-                }
+            }
         }
     }
 
     /**
-   * Método para crear un nuevo registro en el historial.
-   * @param req - Objeto Request de Express.
-   * @param res - Objeto Response de Express.
-   */
+     * Método para crear un nuevo registro en el historial.
+     * @param req - Objeto Request de Express.
+     * @param res - Objeto Response de Express.
+     */
     private async postCreateHistory(req: Request, res: Response) {
         try {
-            const userId = req.params.ID_usuario;// Obtiene el ID del usuario desde los parámetros de la URL
+            const userId = req.params.ID_usuario; // Obtiene el ID del usuario desde los parámetros de la URL
 
             // Agregar el ID_usuario al cuerpo del historial antes de crearlo
             const historyData = {
@@ -137,9 +146,9 @@ class HistoryController extends AbstractController {
             await db["historial"].create(historyData);
             console.log("Registro exitoso");
             res.status(200).send("Registro exitoso");
-        } catch(err:any) {
-            console.log("Error")
-            res.status(500).send("Error fatal:" +err);
+        } catch (err: any) {
+            console.log("Error");
+            res.status(500).send("Error fatal: " + err);
         }
     }
 }
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,76 +3,79 @@ import AbstractController from "./AbstractController";
 import db from '../models';
 import fetch from "node-fetch";
 
-class UserController extends AbstractController{
+class UserController extends AbstractController {
     protected validateBody(type: any) {
-            throw new Error("Method not implemented.");
+        throw new Error("Method not implemented.");
     }
-    //Singleton
-    //Atributo de clase
-    private static instance:UserController;
-    //Método de clase
-    public static getInstance():AbstractController{
-            if(this.instance){
+
+    // Singleton
+    // Atributo de clase
+    private static instance: UserController;
+
+    // Método de clase
+    public static getInstance(): AbstractController {
+        if (this.instance) {
             return this.instance;
-            }
-            this.instance = new UserController('user');
-            return this.instance;
+        }
+        this.instance = new UserController('user');
+        return this.instance;
     }
+
     protected initRoutes(): void {
-            this.router.get('/readUsers',this.getReadUsers.bind(this));
-            this.router.post('/createUser',this.postCreateUser.bind(this));
-            this.router.get('/readUser',this.getReadUser.bind(this));
-            this.router.post('/loginUser',this.getReadUserByEmail.bind(this));
-            this.router.get('/updateUser',this.updateUser.bind(this));
-            this.router.get('/deleteUser',this.deleteUser.bind(this));
-            this.router.get('/updateCashback',this.updateUserCashback.bind(this));
-            //Todas las rutas que necesite su controlador
+        this.router.get('/readUsers', this.getReadUsers.bind(this));
+        this.router.post('/createUser', this.postCreateUser.bind(this));
+        this.router.get('/readUser', this.getReadUser.bind(this));
+        this.router.post('/loginUser', this.getReadUserByEmail.bind(this));
+        this.router.get('/updateUser', this.updateUser.bind(this));
+        this.router.get('/deleteUser', this.deleteUser.bind(this));
+        this.router.get('/updateCashback', this.updateUserCashback.bind(this));
+        // Todas las rutas que necesite su controlador
     }
 
-    //Servicio para obtener todos los usuarios
-    private async getReadUsers(req:Request,res:Response){
-            try{
-                    let usuarios= await db["usuario"].findAll()
-                    console.log("Usuario:", usuarios);
-                    res.send(usuarios);
-            
-            }catch(error){
-                    if (error instanceof Error){
-                    res.status(500).send({ message: error.message });
-                    }else{
-                    res.status(500).send({ message: "Error" });
-                    }
+    // Servicio para obtener todos los usuarios
+    private async getReadUsers(req: Request, res: Response) {
+        try {
+            let usuarios = await db["usuario"].findAll();
+            console.log("Usuario:", usuarios);
+            res.send(usuarios);
+
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(500).send({ message: error.message });
+            } else {
+                res.status(500).send({ message: "Error" });
             }
-    
+        }
+
     }
 
-    //Servicio para crear un registro de usuario nuevo
-    private async postCreateUser(req:Request,res:Response){
-            try{
-                    console.log(req.body);
-                    await db["usuario"].create(req.body);
-                    console.log("Registro exitoso");
-                    res.status(200).send("Registro exitoso");
-            }catch(err:any){
-                    console.log("Error")
-                    res.status(500).send("Error fatal:" +err); 
-            }
+    // Servicio para crear un registro de usuario nuevo
+    private async postCreateUser(req: Request, res: Response) {
+        try {
+            console.log(req.body);
+            await db["usuario"].create(req.body);
+            console.log("Registro exitoso");
+            res.status(200).send("Registro exitoso");
+        } catch (err: any) {
+            console.log("Error");
+            res.status(500).send("Error fatal:" + err);
+        }
     }
 
-    //Servicio para obtener el registro de un usuario
+    // Servicio para obtener el registro de un usuario
     private async getReadUser(req: Request, res: Response) {
         try {
             const userId = req.body.ID_usuario;
 
-            //Revisa si se encuentra el parámetro dentro del cuerpo
+            // Revisa si se encuentra el parámetro dentro del cuerpo
             if (!userId) {
                 res.status(400).send({ message: "Falta parámetro de correo" });
                 return;
             }
-    
+
             // Obtener un registro en el que el correo sea el mismo que en la petición
             const user = await db["usuario"].findOne({ where: { ID_usuario: userId } });
-    
+
             if (!user) {
                 // Si el usuario con ese correo no existe
                 res.status(404).send({ message: "Usuario no encontrado" });
@@ -80,7 +83,7 @@ class UserController extends AbstractController{
             }
 
             console.log("User:", user);
-    
+
             // Envía la información obtenida como respuesta
             res.send(user);
 
@@ -93,24 +96,24 @@ class UserController extends AbstractController{
         }
     }
 
-    //Servicio para obtener el registro de un usuario
+    // Servicio para obtener el registro de un usuario
     private async getReadUserByEmail(req: Request, res: Response) {
         try {
             const userEmail = req.body.Correo;
             const userPass = req.body.Contrasena;
             const { Op } = require("sequelize");
 
-            console.log(req.body)
+            console.log(req.body);
 
-            //Revisa si se encuentra el parámetro dentro del cuerpo
+            // Revisa si se encuentra el parámetro dentro del cuerpo
             if (!userEmail) {
                 res.status(400).send({ message: "Falta parámetro de correo" });
                 return;
             }
-    
+
             // Obtener un registro en el que el correo sea el mismo que en la petición
-            const user = await db["usuario"].findOne({where: {[Op.and]: [{ Correo: userEmail},{ Contrasena: userPass}]}});
-    
+            const user = await db["usuario"].findOne({ where: { [Op.and]: [{ Correo: userEmail }, { Contrasena: userPass }] } });
+
             if (!user) {
                 // Si el usuario con ese correo no existe
                 res.status(404).send({ message: "Usuario no encontrado" });
@@ -118,7 +121,7 @@ class UserController extends AbstractController{
             }
 
             console.log("User:", user);
-    
+
             // Envía la información obtenida como respuesta
             res.send(user);
 
@@ -131,55 +134,55 @@ class UserController extends AbstractController{
         }
     }
 
-    //Servicio para actualizar la información de un usuario
+    // Servicio para actualizar la información de un usuario
     private async updateUser(req: Request, res: Response) {
         try {
 
             const userID = req.body.ID_usuario;
 
-            //Revisa si se encuentra el parámetro dentro del cuerpo
+            // Revisa si se encuentra el parámetro dentro del cuerpo
             if (!userID) {
                 res.status(400).send({ message: "Falta parámetro de correo" });
                 return;
             }
-    
+
             const updatedUserData = req.body;
-    
+
             if (!updatedUserData) {
                 res.status(400).send({ message: "No se encuentra la información nueva del usuario" });
                 return;
             }
-    
+
             // Encuentra el registro de correo para el usuario
             const user = await db["usuario"].findOne({ where: { ID_usuario: userID } });
-    
+
             if (!user) {
-                    // If the user with the specified email is not found, return a 404 status
-                    res.status(404).send({ message: "No se encuentra el usuario" });
-                    return;
+                // If the user with the specified email is not found, return a 404 status
+                res.status(404).send({ message: "No se encuentra el usuario" });
+                return;
             }
-    
+
             // Update the user with the new data
             await user.update(updatedUserData);
-    
+
             // Log the updated user to the console for debugging
             console.log("Updated User:", user);
-    
+
             // Send a success response
             res.send({ message: "User updated successfully" });
         } catch (error) {
-        // Handle errors
-        if (error instanceof Error) {
+            // Handle errors
+            if (error instanceof Error) {
                 // If the error is an instance of the Error class, send the error message
                 res.status(500).send({ message: error.message });
-        } else {
+            } else {
                 // If it's not an instance of Error (unexpected case), send a generic error message
                 res.status(500).send({ message: "Error" });
-        }
+            }
         }
     }
 
-    //Servicio para borrar un usuario
+    // Servicio para borrar un usuario
     private async deleteUser(req: Request, res: Response) {
         try {
             // Assuming you have an email parameter in the request
@@ -187,17 +190,17 @@ class UserController extends AbstractController{
 
             // Check if userID is undefined or null
             if (!userID) {
-                    res.status(400).send({ message: "Email parameter is missing" });
-                    return;
+                res.status(400).send({ message: "Email parameter is missing" });
+                return;
             }
 
             // Find the user by email from the "usuario" table
             const user = await db["usuario"].findOne({ where: { ID_usuario: userID } });
 
             if (!user) {
-                    // If the user with the specified email is not found, return a 404 status
-                    res.status(404).send({ message: "User not found" });
-                    return;
+                // If the user with the specified email is not found, return a 404 status
+                res.status(404).send({ message: "User not found" });
+                return;
             }
 
             // Delete the user from the database
@@ -209,49 +212,49 @@ class UserController extends AbstractController{
             // Send a success response
             res.send({ message: "User deleted successfully" });
         } catch (error) {
-        // Handle errors
-        if (error instanceof Error) {
+            // Handle errors
+            if (error instanceof Error) {
                 // If the error is an instance of the Error class, send the error message
                 res.status(500).send({ message: error.message });
-        } else {
+            } else {
                 // If it's not an instance of Error (unexpected case), send a generic error message
                 res.status(500).send({ message: "Error" });
-        }
+            }
         }
     }
 
     private async updateUserCashback(req: Request, res: Response) {
         try {
             const userID = req.body.ID_usuario;
-    
+
             if (!userID) {
                 res.status(400).send({ message: "Missing user parameter (ID_usuario)" });
                 return;
             }
-    
+
             const newCashback = req.body.Cashback;
-    
+
             if (!newCashback) {
                 res.status(400).send({ message: "New user information (Cashback) not found" });
                 return;
             }
-    
+
             const user = await db["usuario"].findOne({ where: { ID_usuario: userID } });
-    
+
             if (!user) {
                 res.status(404).send({ message: "User not found" });
                 return;
             }
-    
+
             user.Cashback = newCashback;
             await user.save(); // Assuming the user model has a save() method
-    
+
             console.log("Updated User:", user.toJSON()); // Log the entire updated user object
-    
+
             res.send({ message: "User updated successfully" });
         } catch (error) {
             console.error('Error updating user cashback:', error);
-    
+
             if (error instanceof Error) {
                 res.status(500).send({ message: error.message });
             } else {
@@ -259,11 +262,6 @@ class UserController extends AbstractController{
             }
         }
     }
-    
-
-    
-
-
 }
 
-export default UserController;  
+export default UserController;
